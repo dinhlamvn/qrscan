b8bc0457ed555a71153fa6ab875008a9d1a0e91a
Dinh Lam <dinhlamvn353@gmail.com>
2022-02-19 15:02:26 +0700

    Add url use case

--- app/src/main/java/android/vn/leo/qrscan/dialog/bottomsheet/coderesult/dialog/UrlResultDialogFragment.kt
+++ app/src/main/java/android/vn/leo/qrscan/dialog/bottomsheet/coderesult/dialog/UrlResultDialogFragment.kt
@@ -1,5 +1,6 @@
 package android.vn.leo.qrscan.dialog.bottomsheet.coderesult.dialog
 
+import android.net.Uri
 import android.os.Bundle
 import android.vn.leo.qrscan.R
 import android.vn.leo.qrscan.dialog.bottomsheet.coderesult.BaseCodeResultDialogFragment
@@ -7,10 +8,10 @@ import android.vn.leo.qrscan.dialog.bottomsheet.coderesult.OnResultDialog
 import android.vn.leo.qrscan.dialog.listener.OnDialogDismissListener
 import android.vn.leo.qrscan.extensions.bindView
 import android.vn.leo.qrscan.model.BarcodeParsedResult
-import android.vn.leo.qrscan.utils.CommonMethod
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.FragmentManager
 import com.bumptech.glide.Glide
 
@@ -23,11 +24,13 @@ class UrlResultDialogFragment : BaseCodeResultDialogFragment<BarcodeParsedResult
 
     private val ivScanResult: ImageView by bindView(R.id.image_review_scan_result)
     private val tvScanResult: TextView by bindView(R.id.text_view_scan_result)
-    private val btnCopyAndClose: Button by bindView(R.id.button_copy)
+    private val btnAccess: Button by bindView(R.id.button_access)
     private val btnDismiss: Button by bindView(R.id.button_dismiss)
 
+    private val customTabBuilder = CustomTabsIntent.Builder()
+
     override val layoutRes: Int
-        get() = R.layout.fragment_dialog_text_result
+        get() = R.layout.fragment_dialog_url_result
 
     override fun setupUI(result: BarcodeParsedResult.UrlResult) {
         Glide.with(this)
@@ -36,9 +39,9 @@ class UrlResultDialogFragment : BaseCodeResultDialogFragment<BarcodeParsedResult
 
         tvScanResult.text = result.url
 
-        btnCopyAndClose.setOnClickListener {
-            CommonMethod.copyResultToClipboard(requireContext(), result.text)
-            dismiss()
+        btnAccess.setOnClickListener {
+            customTabBuilder.build()
+                .launchUrl(requireContext(), Uri.parse(result.url))
         }
 
         btnDismiss.setOnClickListener {
